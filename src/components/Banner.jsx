61b585c85f2480edc8ce2788569fbcacd6f4fccb
ruksat13 <mdruksathasanakib@gmail.com>
2026-05-17 06:51:02 +0600

    fix: banner vector image positioning

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,18 @@
 import "./Banner.css";
 import vector1 from "../assets/vector1.png";
-import vector2 from "../assets/vector2.png";
 
 const Banner = ({ inProgress, resolved }) => {
     return (
         <div className="banner">
             <div className="stat-card stat-progress">
-                <img src={vector1} alt="" className="vector-img" />
+                <img src={vector1} alt="" className="vector-img vector-right" />
+                <img src={vector1} alt="" className="vector-img vector-left" />
                 <p className="stat-label">In-Progress</p>
                 <h2 className="stat-number">{inProgress}</h2>
             </div>
             <div className="stat-card stat-resolved">
-                <img src={vector2} alt="" className="vector-img" />
+                <img src={vector1} alt="" className="vector-img vector-right" />
+                <img src={vector1} alt="" className="vector-img vector-left" />
                 <p className="stat-label">Resolved</p>
                 <h2 className="stat-number">{resolved}</h2>
             </div>
